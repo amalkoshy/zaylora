cf22abb5434483b041b3f827400ff014d8849b3c
amal <amalkoshy@gmail.com>
2026-06-13 12:23:32 +0530

    address updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
                             </svg>
-                            <span>Zaylora spices,<br>Kuruvilla City, Rajakumari, Kerala 685619</span>
+                            <span>Zaylora spices,<br>Mammattikanam P.O, road, Pazhayaviduthi, Rajakkad, Kuthumkal, Kerala 685566</span>
                         </li>
                     </ul>
                 </div>
